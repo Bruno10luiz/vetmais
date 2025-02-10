884abe41155a377d04875bfaa70e8847553017bb
Bruno Luiz Camargo <bruno10luiz@gmail.com>
2025-02-09 22:20:07 -0300

    vetmais

---
+++ src/app/_components/about.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import about1Img from "../../../public/about-1.png";
+import about2Img from "../../../public/about-2.png";
+import { Check, MapPin } from "lucide-react";
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
+
+export function About() {
+  return (
+    <section className="bg-[#FDF6EC] py-16">
+      <div className="container px-4 mx-auto ">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+          <div className="relative ">
+            <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
+              <Image
+                src={about1Img}
+                alt="Foto de cachorro"
+                fill
+                quality={100}
+                className="object-cover hover:scale-110 duration-300"
+                priority
+              />
+            </div>
+
+            <div className="absolute w-40 h-40 right-4 -bottom-8 border-4 overflow-hidden rounded-lg border-white">
+              <Image
+                src={about2Img}
+                alt="Foto de cachorro 2"
+                fill
+                quality={100}
+                priority
+              />
+            </div>
+          </div>
+
+          <div className="space-y-6 mt-10">
+            <h2 className="text-4xl font-bold"> SOBRE</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur saepe sint voluptatum tenetur tempora ut, minus
+              reiciendis sequi earum veniam? Quos alias fugiat eius tempora
+              accusamus qui, quas consequuntur ad dolores
+            </p>
+            <ul className="space-y-4">
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Lorem ipsum dolor sit amet consectetur adipisicing
+              </li>
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Lorem ipsum dolor sit amet consectetur adipisicing
+              </li>
+              <li className="flex items-center gap-2">
+                <Check className="text-red-500" />
+                Lorem ipsum dolor sit amet consectetur adipisicing
+              </li>
+            </ul>
+            <div className="flex gap-2">
+              <a
+                href="#"
+                className="bg-[#3de85a] text white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
+              >
+                <WhatsappLogo className="w-5 h-5 text-white " />
+                Contato via Whatsapp
+              </a>
+              <a
+                href="#"
+                className=" text white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
+              >
+                <MapPin className="w-5 h-5 text-black " />
+                Endereço da Clínica
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/_components/address.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { Loader } from "googlemaps/js-api-loader";
+
+export function Address() {
+  return;
+}
